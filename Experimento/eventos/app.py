309745a1d3e82be026640f18dfd3349425b70fc4
gbrandon3 <b.gonzalezp@uniandes.edu.co>
2023-07-26 13:45:33 -0500

    Agregado docker file para microservicio eventos

--- Experimento/eventos/app.py
+++ Experimento/eventos/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -6,7 +7,7 @@ from flask_restful import Api
 from modelos import db
 from vistas import VistaGetCarrera,VistaCrearCarrera,VistaEditarCarrera,VistaEliminarCarrera
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eporra.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI','sqlite:///eporra.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
